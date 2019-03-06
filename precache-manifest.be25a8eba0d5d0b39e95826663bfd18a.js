4cc8c672bf2c9c992da14938941e1c369698404a
atishayjain708 <atishayjain708@gmail.com>
2019-03-06 18:15:32 +0530

    Updates

---
+++ precache-manifest.be25a8eba0d5d0b39e95826663bfd18a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "75f565eb15b4689f8fa6",
+    "url": "/portfolio/static/js/main.75f565eb.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "75f565eb15b4689f8fa6",
+    "url": "/portfolio/static/css/main.596a83af.chunk.css"
+  },
+  {
+    "revision": "bd1a7e1edd0b76ba3a7ab9e642864a1a",
+    "url": "/portfolio/index.html"
+  }
+];
